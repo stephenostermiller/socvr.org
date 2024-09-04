78f4756b57d6c54028b0e4fb7762b47afbe4e31c
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2024-09-04 03:29:27 -0400

    Combine js into one file

--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
 "use strict"
 
-document.addEventListener("DOMContentLoaded", function(){
-	function menu(){
-		var path = location.pathname
-		path = path.replace(/[^\/]*$/g,"")
-		while (path != ""){
-			const nav = document.createElement("nav")
-			fetch(location.origin+"/pages"+path+"_nav.csv").then(function(response){
-				response.text().then(function(text){
-					text.split(/\n/).forEach(function(line){
-						var m = line.match('^([^,#]+),([a-z0-9.\/\-]+)$')
-						if (m){
-							var href = m[2]
-							var link = document.createElement("a")
-							link.classList.add("nav-item>")
-							link.innerText = m[1]
-							link.setAttribute('href',href)
-							if (location.pathname.startsWith(href)) link.classList.add('selected')
-							if (location.pathname == href) link.classList.add('current')
-							nav.append(link)
-						}
-					})
-				})				
-			})
-			document.getElementById("navigation").prepend(nav)
-			path = path.replace(/[^\/]*\/$/g,"")
-		}		
+document.addEventListener("DOMContentLoaded", function(){	
+	// Menu
+	var path = location.pathname
+	path = path.replace(/[^\/]*$/g,"")
+	while (path != ""){
+		const nav = document.createElement("nav")
+		fetch(location.origin+"/pages"+path+"_nav.csv").then(function(response){
+			response.text().then(function(text){
+				text.split(/\n/).forEach(function(line){
+					var m = line.match('^([^,#]+),([a-z0-9.\/\-]+)$')
+					if (m){
+						var href = m[2]
+						var link = document.createElement("a")
+						link.classList.add("nav-item>")
+						link.innerText = m[1]
+						link.setAttribute('href',href)
+						if (location.pathname.startsWith(href)) link.classList.add('selected')
+						if (location.pathname == href) link.classList.add('current')
+						nav.append(link)
+					}
+				})
+			})				
+		})
+		document.getElementById("navigation").prepend(nav)
+		path = path.replace(/[^\/]*\/$/g,"")
 	}
-	menu()
+
+	// Client side routing and markdown
+	var url = location.origin + "/"
+	var path = location.pathname.replace(/\.html$/,"")
+	if (/\/$/.test(path)) path += "index"
+	var mdFile = "/pages" + path + ".md"
+	fetch(mdFile).then(response=>response.text()).then(text=>{
+		var m
+		if (m = /^# ([^\r\n]*)/.exec(text)){
+			document.querySelector('title').innerText = m[1] + " | socvr.org"
+		}
+		document.getElementById('content').innerHTML = window.markdownit({html:true}).render(text).replace(/\$URL\$/,url)
+	})
 })
